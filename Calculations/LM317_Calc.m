13c921a1e4935583be916165055391551537b4a2
Michael Tisdale <mltisdale@ucdavis.edu>
2026-04-11 20:37:02 -0700

    First draft of schematic finished

--- Calculations/LM317_Calc.m
+++ Calculations/LM317_Calc.m
@@ -1,4 +1,7 @@
-Vout = [1.5 3.3 5 9 10 12];
-R1 = 240;
-R2 = (R1/1.25).*(Vout - 1.25);
-disp(R2)
+Vout = [1.5 3.3 5 9 10 12]; % desired output voltages
+R1 = 240; % resistor between adjust pin and out pin (leave this)
+R2 = (R1 ./ 1.25) .* (Vout - 1.25); % calculation for the 'adjust' resistor
+
+for i = 1:length(Vout)
+    fprintf('For %.1fV the Resistance Needed is %.2f ohms\n', Vout(i), R2(i));
+end
