50e2cd1e1c2a8762ed84c7f725ff647eac72a339
Michael Tisdale <mltisdale@ucdavis.edu>
2026-04-13 15:59:49 -0700

    Created a mini sim that replacates LM317 behavior

--- Calculations/LM317_Calc.m
+++ Calculations/LM317_Calc.m
@@ -1,4 +1,4 @@
-Vout = [1.5 3.3 5 9 10 12]; % desired output voltages
+Vout = [1.5 3 3.3 5 6 9 10]; % desired output voltages
 R1 = 240; % resistor between adjust pin and out pin (leave this)
 R2 = (R1 ./ 1.25) .* (Vout - 1.25); % calculation for the 'adjust' resistor
 
